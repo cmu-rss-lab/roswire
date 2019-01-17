fc445bd5be9dda26509f8a90262a556acdd2200c
ChrisTimperley <christimperley@googlemail.com>
2019-01-17 18:54:43 -0500

    implemented basic shell proxy

--- src/rozzy/container.py
+++ src/rozzy/container.py
@@ -1,9 +1,33 @@
-__all__ = ['Container']
+__all__ = ['Container', 'ShellProxy']
 
+from typing import Optional, Tuple
 from uuid import UUID
 
 from bugzoo import BugZoo as BugZooDaemon
 from bugzoo import Container as BugZooContainer
+
+
+class ShellProxy(object):
+    def __init__(self,
+                 daemon_bugzoo: BugZooDaemon,
+                 container_bugzoo: BugZooContainer
+                 ) -> None:
+        self.__daemon_bugzoo = daemon_bugzoo
+        self.__container_bugzoo = container_bugzoo
+
+    def execute(self,
+                command: str,
+                context: Optional[str] = None,
+                stdout: bool = True,
+                stderr: bool = False
+                ) -> Tuple[int, str, float]:
+        mgr = self.__daemon_bugzoo.containers
+        r = mgr.command(self.__container_bugzoo,
+                        command,
+                        context=context,
+                        stdout=stdout,
+                        stderr=stderr)
+        return (r.code, r.output, r.duration)
 
 
 class Container(object):
@@ -15,3 +39,8 @@ class Container(object):
         self.__daemon_bugzoo = daemon_bugzoo
         self.__container_bugzoo = container_bugzoo
         self.__uuid = uuid
+
+    @property
+    def shell(self) -> ShellProxy:
+        return ShellProxy(self.__daemon_bugzoo,
+                          self.__container_bugzoo)
