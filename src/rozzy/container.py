dd65a2f3a8d4097841e7aa63515409b289006e57
ChrisTimperley <christimperley@googlemail.com>
2019-01-17 18:31:48 -0500

    removed is_alive and _close from Container

--- src/rozzy/container.py
+++ src/rozzy/container.py
@@ -12,16 +12,6 @@ class Container(object):
                  container_bugzoo: BugZooContainer,
                  uuid: UUID
                  ) -> None:
-        self.__is_alive = True
         self.__daemon_bugzoo = daemon_bugzoo
         self.__container_bugzoo = container_bugzoo
         self.__uuid = uuid
-
-    @property
-    def is_alive(self) -> bool:
-        return self.__is_alive
-
-    def _close(self) -> None:
-        if self.__is_alive:
-            del self.__daemon_bugzoo.containers[self.__container_bugzoo.id]
-            self.__is_alive = False
