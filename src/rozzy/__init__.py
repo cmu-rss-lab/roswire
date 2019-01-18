e8a05c7af198fdf175f0ae302c1266fed7d721a5
ChrisTimperley <christimperley@googlemail.com>
2019-01-17 21:19:27 -0500

    moved ShellProxy to proxy module

--- src/rozzy/__init__.py
+++ src/rozzy/__init__.py
@@ -1,3 +1,4 @@
 from .version import __version__
+from .proxy import ShellProxy
 from .container import Container
 from .rozzy import Rozzy
--- src/rozzy/container.py
+++ src/rozzy/container.py
@@ -1,33 +1,11 @@
-__all__ = ['Container', 'ShellProxy']
+__all__ = ['Container']
 
-from typing import Optional, Tuple
 from uuid import UUID
 
 from bugzoo import BugZoo as BugZooDaemon
 from bugzoo import Container as BugZooContainer
-from bugzoo.cmd import PendingExecResponse
 
-
-class ShellProxy(object):
-    def __init__(self,
-                 daemon_bugzoo: BugZooDaemon,
-                 container_bugzoo: BugZooContainer
-                 ) -> None:
-        self.__daemon_bugzoo = daemon_bugzoo
-        self.__container_bugzoo = container_bugzoo
-
-    def execute(self, command: str, **kwargs) -> Tuple[int, str, float]:
-        mgr = self.__daemon_bugzoo.containers
-        r = mgr.command(self.__container_bugzoo, command, **kwargs)
-        return r.code, r.output, r.duration
-
-    def non_blocking_execute(self,
-                             command: str,
-                             **kwargs
-                             ) -> PendingExecResponse:
-        mgr = self.__daemon_bugzoo.containers
-        r = mgr.command(self.__container_bugzoo, command, block=False, **kwargs)
-        return r
+from .proxy import ShellProxy
 
 
 class Container(object):
