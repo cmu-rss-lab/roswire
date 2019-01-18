a710996f441b4d5597e78e46eafe6726cacae339
ChrisTimperley <christimperley@googlemail.com>
2019-01-17 23:23:41 -0500

    added ip_address property to Container

--- src/rozzy/container.py
+++ src/rozzy/container.py
@@ -1,11 +1,13 @@
 __all__ = ['Container']
 
+from typing import Iterator
 from uuid import UUID
 
 from bugzoo import BugZoo as BugZooDaemon
 from bugzoo import Container as BugZooContainer
 
-from .proxy import ShellProxy
+from .exceptions import RozzyException
+from .proxy import ShellProxy, ROSProxy
 
 
 class Container(object):
@@ -17,7 +19,22 @@ class Container(object):
         self.__daemon_bugzoo = daemon_bugzoo
         self.__container_bugzoo = container_bugzoo
         self.__uuid = uuid
+        self.__shell = ShellProxy(daemon_bugzoo, container_bugzoo)
+
+    @property
+    def uuid(self) -> UUID:
+        return self.__uuid
+
+    @property
+    def ip_address(self) -> str:
+        bz = self.__daemon_bugzoo
+        ip = bz.containers.ip_address(self.__container_bugzoo)
+        if not ip:
+            m = "no IP address for container: {}"
+            m = m.format(self.uuid.hex)
+            raise RozzyException(m)
+        return str(ip)
 
     @property
     def shell(self) -> ShellProxy:
-        return ShellProxy(self.__daemon_bugzoo, self.__container_bugzoo)
+        return self.__shell
