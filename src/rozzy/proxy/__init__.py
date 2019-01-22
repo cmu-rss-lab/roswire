b14031135f40032f0594a3ff9bd3650068600821
ChrisTimperley <christimperley@googlemail.com>
2019-01-22 15:18:01 -0500

    split shell proxy into its own module

--- src/rozzy/proxy/__init__.py
+++ src/rozzy/proxy/__init__.py
@@ -6,39 +6,11 @@ import xmlrpc.client
 import logging
 import time
 
-from bugzoo import BugZoo as BugZooDaemon
-from bugzoo import Container as BugZooContainer
-from bugzoo.cmd import PendingExecResponse
-
+from .shell import ShellProxy
 from ..exceptions import RozzyException
 
 logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
-
-
-class ShellProxy(object):
-    """
-    Provides shell access for a given BugZoo container.
-    """
-    def __init__(self,
-                 daemon_bugzoo: BugZooDaemon,
-                 container_bugzoo: BugZooContainer
-                 ) -> None:
-        self.__daemon_bugzoo = daemon_bugzoo
-        self.__container_bugzoo = container_bugzoo
-
-    def execute(self, command: str, **kwargs) -> Tuple[int, str, float]:
-        mgr = self.__daemon_bugzoo.containers
-        r = mgr.command(self.__container_bugzoo, command, **kwargs)
-        return r.code, r.output, r.duration
-
-    def non_blocking_execute(self,
-                             command: str,
-                             **kwargs
-                             ) -> PendingExecResponse:
-        mgr = self.__daemon_bugzoo.containers
-        r = mgr.command(self.__container_bugzoo, command, block=False, **kwargs)
-        return r
 
 
 class ParameterServerProxy(object):
