1f725611dcda33c3668c5bb2792f4b47fdfa384e
ChrisTimperley <christimperley@googlemail.com>
2019-02-27 19:40:44 -0500

    added test_parameters

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,54 +1,65 @@
+from typing import Iterator
+import contextlib
 import logging
 import time
 
-from rozzy import Rozzy
+import pytest
+
+from rozzy import Rozzy, ROSProxy
+
+
+@contextlib.contextmanager
+def build_test_environment() -> Iterator[ROSProxy]:
+    rozzy = Rozzy()
+    with rozzy.launch() as container:
+        with container.roscore() as ros:
+            time.sleep(5)
+            yield ros
+
+
+def test_parameters():
+    with build_test_environment() as ros:
+        assert ros.topic_to_type == {'/rosout': 'rosgraph_msgs/Log',
+                                     '/rosout_agg': 'rosgraph_msgs/Log'}
+
+        assert '/rosversion' in ros.parameters
+        assert '/rosdistro' in ros.parameters
+
+        assert '/hello' not in ros.parameters
+        ros.parameters['/hello'] = 'world'
+        assert '/hello' in ros.parameters
+        assert ros.parameters['/hello'] == 'world'
+
+        del ros.parameters['/hello']
+        assert 'hello' not in ros.parameters
+        with pytest.raises(KeyError):
+            ros.parameters['/hello']
 
 
 def main():
     logging.basicConfig()
-    rozzy = Rozzy()
-    with rozzy.launch() as container:
-        print(f"IP address: {container.ip_address}")
-        with container.roscore() as ros:
-            print(f"ROS Master URI: {ros.uri}")
-            time.sleep(10)
-            print(ros.topic_to_type)
-            # print(ros.lookup_node('rosout'))
+    with build_test_environment() as ros:
+        # TODO connect to simulator: allow visualisation
+        # launch SITL on 5760
+        # sim_vehicle.py -C -v ArduCopter --daemon --no-rebuild
+        cmd = ' '.join([
+            "/ros_ws/src/ArduPilot/build/sitl/bin/arducopter",
+            "--model copter"
+        ])
+        container.shell.non_blocking_execute(cmd)
 
-            """
-            ros.parameters['/chris'] = 'hello!'
-            print(list(ros.parameters))
-            print(f"/rosversion: {ros.parameters['/rosversion']}")
-            print(f"/chris: {ros.parameters['/chris']}")
-            print(f"contains /chris? {'yes' if '/chris' in ros.parameters else 'no'}")
-            print(f"contains /pikachu? {'yes' if '/pikachu' in ros.parameters else 'no'}")
-            del ros.parameters['/rosversion']
-            print(list(ros.parameters))
-            """
+        # launch mavros
+        cmd = ' '.join([
+            "roslaunch",
+            "mavros apm.launch",
+            "fcu_url:=tcp://127.0.0.1:5760@5760"
+        ])
+        container.shell.non_blocking_execute(cmd)
 
-            # TODO connect to simulator: allow visualisation
-
-            # launch SITL on 5760
-            # sim_vehicle.py -C -v ArduCopter --daemon --no-rebuild
-            cmd = ' '.join([
-                "/ros_ws/src/ArduPilot/build/sitl/bin/arducopter",
-                "--model copter"
-            ])
-            container.shell.non_blocking_execute(cmd)
-
-            # launch mavros
-            cmd = ' '.join([
-                "roslaunch",
-                "mavros apm.launch",
-                "fcu_url:=tcp://127.0.0.1:5760@5760"
-            ])
-            container.shell.non_blocking_execute(cmd)
-
-            time.sleep(30)
-
-            # launch_mavros = ros.launch("mavros apm.launch",
-            #                            {'fcu_url': 'tcp://127.0.0.1:5760:5760'})
+        time.sleep(30)
 
 
-if __name__ == '__main__':
-    main()
+        print(ros.topic_to_type)
+
+        # launch_mavros = ros.launch("mavros apm.launch",
+        #                            {'fcu_url': 'tcp://127.0.0.1:5760:5760'})
