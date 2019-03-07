2012748b0adf334114e5779a3740e20e7be05648
ChrisTimperley <christimperley@googlemail.com>
2019-03-07 17:49:10 -0500

    added exists method to FileProxy

--- src/rozzy/proxy/file.py
+++ src/rozzy/proxy/file.py
@@ -1,5 +1,9 @@
 __all__ = ['FileProxy']
 
+import shlex
+
+from .shell import ShellProxy
+
 from bugzoo import BugZoo as BugZooDaemon
 from bugzoo import Container as BugZooContainer
 
@@ -8,10 +12,20 @@ class FileProxy:
     def __init__(self,
                  daemon_bugzoo: BugZooDaemon,
                  container_bugzoo: BugZooContainer,
-                 ws_host: str
+                 ws_host: str,
+                 shell: ShellProxy
                  ) -> None:
         self.__daemon_bugzoo: BugZooDaemon = daemon_bugzoo
         self.__container_bugzoo: BugZooContainer = container_bugzoo
+        self.__shell: ShellProxy = shell
 
         self.__dir_ws_host: str = ws_host
         self.__dir_ws_container: str = '/.rozzy'
+
+    def exists(self, path: str) -> bool:
+        """
+        Determines whether a file or directory exists at the given path.
+        """
+        cmd = f'test -e "{shlex.quote(path)}"'
+        code, output, duration = self.__shell.execute(cmd)
+        return code == 0
