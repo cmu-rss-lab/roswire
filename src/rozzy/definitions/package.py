bfe556d88e851fcc3c1555dd6b04d6261a8fcac6
ChrisTimperley <christimperley@googlemail.com>
2019-03-08 13:23:55 -0500

    added placeholder build method

--- src/rozzy/definitions/package.py
+++ src/rozzy/definitions/package.py
@@ -1,18 +1,35 @@
 __all__ = ['Package']
 
-from typing import Tuple
+from typing import Tuple, List
+import os
 
 import attr
 
 from .msg import MsgFormat
 from .srv import SrvFormat
 from .action import ActionFormat
+from ..proxy import FileProxy
 
 
 @attr.s(frozen=True)
 class Package:
-    name = attr.ib(type=str)
-    path = attr.ib(type=str)
-    messages = attr.ib(type=Tuple[MsgFormat, ...])
-    services = attr.ib(type=Tuple[SrvFormat, ...])
-    actions = attr.ib(type=Tuple[ActionFormat, ...])
+    name: str = attr.ib()
+    path: str = attr.ib()
+    messages: Tuple[MsgFormat, ...] = attr.ib(converter=tuple)
+    services: Tuple[SrvFormat, ...]  = attr.ib(converter=tuple)
+    actions: Tuple[ActionFormat, ...] = attr.ib(converter=tuple)
+
+    @staticmethod
+    def build(path: str, files: FileProxy) -> 'Package':
+        """
+        Constructs a description of a package located at a given path.
+        """
+        name: str = os.path.basename(path)
+        messages: List[MsgFormat] = []
+        services: List[SrvFormat] = []
+        actions: List[ActionFormat] = []
+        return Package(name,  # type: ignore
+                       path,
+                       messages,
+                       services,
+                       actions)
