5d4b05cbbe27464f89b659481031ac8f9af860f6
ChrisTimperley <christimperley@googlemail.com>
2019-03-13 22:11:38 -0400

    added prototype TypeDatabase

---
+++ src/rozzy/definitions/type_db.py
@@ -0,0 +1,40 @@
+__all__ = ('TypeDatabase', 'Message')
+
+from typing import Collection, Type, Mapping, Iterator, Dict, ClassVar
+
+from .msg import MsgFormat
+from .format import FormatDatabase
+
+
+class Message:
+    """
+    Base class used by all messages.
+    """
+    format: ClassVar[MsgFormat]
+
+
+class TypeDatabase(Mapping[str, Type[Message]]):
+    def build(db_format: FormatDatabase) -> 'TypeDatabase':
+        types = [build_type(m) for m in db_format.messages]
+        return TypeDatabse(types)
+
+    def build_type(fmt: MsgFormat) -> Type[Message]:
+        # FIXME find type
+        ns: Dict[str, Any] = {f.name: attr.ib() for f in fmt.fields}
+        ns['format'] = fmt
+        t: Type[Message] = type(fmt.name, (Message,), ns)
+        t = attr.s(t, frozen=True, slots=True)
+        return t
+
+    def __init__(self, types: Collection[Type[Message]]) -> None:
+        self.__contents: Dict[str, Type[Message]] = \
+            {t.format.fullname: t for t in types}
+
+    def __len__(self) -> int:
+        return len(self.__contents)
+
+    def __iter__(self) -> Iterator[str]:
+        return self.__contents.__iter__()
+
+    def __getitem__(self, name: str) -> Type[Message]:
+        return self.__contents[name]
