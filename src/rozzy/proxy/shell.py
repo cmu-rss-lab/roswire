b82e8eaf8d74ce2374f6f2878f3faf81f5107486
ChrisTimperley <christimperley@googlemail.com>
2019-03-15 16:13:23 -0400

    began updating shell proxy

--- src/rozzy/proxy/shell.py
+++ src/rozzy/proxy/shell.py
@@ -1,6 +1,7 @@
 __all__ = ['ShellProxy']
 
-from typing import Tuple
+from typing import Tuple, Optional
+import shlex
 
 from bugzoo import BugZoo as BugZooDaemon
 from bugzoo import Container as BugZooContainer
@@ -18,9 +19,39 @@ class ShellProxy:
         self.__daemon_bugzoo = daemon_bugzoo
         self.__container_bugzoo = container_bugzoo
 
-    def execute(self, command: str, **kwargs) -> Tuple[int, str, float]:
-        mgr = self.__daemon_bugzoo.containers
-        r = mgr.command(self.__container_bugzoo, command, **kwargs)
+    def instrument(self,
+                   command: str,
+                   context: str,  # TODO may not be needed
+                   time_limit: Optional[int] = None,
+                   kill_after: int = 1
+                   ) -> str:
+        q = shlex.quote
+        command = 'source /.environment && cd {q(context)} && {command}'
+        command = '/bin/bash -c {q(command)}'
+        if time_limit:
+            command = (f'timeout --kill-after={kill_after} '
+                       f'--signal=SIGTERM {time_limit} {command}')
+        return command
+
+    def execute(self,
+                command: str,
+                *,
+                stdout: bool = True,
+                stderr: bool = True,
+                user: Optional[str] = None,
+                context: Optional[str] = None,
+                time_limit: Optional[int] = None,
+                kill_after: int = 1
+                ) -> Tuple[int, str, float]:
+        command = self.instrument(command, context, time_limit, kill_after)
+
+        # TODO use timer
+        # https://docker-py.readthedocs.io/en/stable/containers.html
+        retcode, output = dockerc.exec_run(cmd,
+                         stdout=stdout,
+                         stderr=stderr)
+
+
         return r.code, r.output, r.duration
 
     def non_blocking_execute(self,
