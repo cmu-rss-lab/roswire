9be3465db184259f0710cac836d35fdd8d16bfba
ChrisTimperley <christimperley@googlemail.com>
2019-03-19 16:59:57 -0400

    added --once parameter to publish

--- src/rozzy/proxy/rostopic.py
+++ src/rozzy/proxy/rostopic.py
@@ -1,11 +1,15 @@
 __all__ = ('ROSTopicProxy',)
 
 import yaml
+import logging
 
 from .shell import ShellProxy
 from .. import exceptions
 from ..description import SystemDescription
 from ..definitions import Message
+
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class ROSTopicProxy:
@@ -17,8 +21,15 @@ class ROSTopicProxy:
         self.__shell = shell
 
     def publish(self, topic: str, message: Message) -> None:
+        """
+        Publishes a given message on a named topic.
+
+        Warning:
+            this method blocks for three seconds as the given message is
+            latched.
+        """
         yml = yaml.dump(message.to_dict())
-        cmd = f"rostopic pub {topic} {message.format.fullname} '{yml}'"
+        cmd = f"rostopic pub --once {topic} {message.format.fullname} '{yml}'"
         retcode, output, duration = self.__shell.execute(cmd)
         if retcode != 0:
             m = 'unexpected error during rostopic pub call'
