c68de042e878a9bf97530f9687d6c123b4bc983a
ChrisTimperley <christimperley@googlemail.com>
2019-03-19 18:30:36 -0400

    added basic Popen

--- src/rozzy/proxy/shell.py
+++ src/rozzy/proxy/shell.py
@@ -1,23 +1,81 @@
 __all__ = ('ShellProxy',)
 
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Dict, Any, Iterator
 import shlex
 import logging
 import threading
 
+from docker import DockerClient
+from docker import APIClient as DockerAPIClient
 from docker.models.containers import Container as DockerContainer
 
 from ..util import Stopwatch
 
-logger = logging.getLogger(__name__)  # type: logging.Logger
+logger: logging.Logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
+
+
+class Popen:
+    def __init__(self,
+                 args: str,
+                 api_docker: DockerAPIClient,
+                 exec_id: int,
+                 stream: Iterator[bytes]
+                 ) -> None:
+        self.__args = args
+        self.__api_docker = api_docker
+        self.__exec_id = exec_id
+        self.__binary = binary
+        self.__stream = stream
+        self.__pid: Optional[int] = None
+        self.__returncode: Optional[int] = None
+
+    def _inspect(self) -> Dict[str, Any]:
+        return self.__api_docker.exec_inspect(self.__exec_id)
+
+    @property
+    def stream(self) -> Iterator[str]:
+        for line_bytes in self.__stream:
+            yield line_bytes.decode('utf-8')
+
+    @property
+    def args(self) -> str:
+        return self.__args
+
+    @property
+    def pid(self) -> Optional[int]:
+        if not self.__pid:
+            pid = self._inspect()['Pid']
+            if pid != 0:
+                self.__pid = pid
+        return self.__pid
+
+    @property
+    def finished(self) -> bool:
+        return self.returncode is not None
+
+    @property
+    def returncode(self) -> Optional[int]:
+        if self.__returncode is None:
+            self.__returncode = self._inspect()['ExitCode']
+        return self.__returncode
+
+    def kill(self) -> None:
+        raise NotImplementedError
+
+    def terminate(self) -> None:
+        raise NotImplementedError
 
 
 class ShellProxy:
     """
     Provides shell access for a given BugZoo container.
     """
-    def __init__(self, container_docker: DockerContainer) -> None:
+    def __init__(self,
+                 api_docker: DockerAPIClient,
+                 container_docker: DockerContainer
+                 ) -> None:
+        self.__api_docker = api_docker
         self.__container_docker = container_docker
 
     def instrument(self,
@@ -34,6 +92,30 @@ class ShellProxy:
                        f'--signal=SIGTERM {time_limit} {command}')
         logger.debug("instrumented command: %s", command)
         return command
+
+    def popen(self,
+              command: str,
+              *,
+              stdout: bool = True,
+              stderr: bool = True,
+              user: Optional[str] = None,
+              context: Optional[str] = None,
+              time_limit: Optional[int] = None,
+              kill_after: int = 1
+              ) -> Tuple[int, str, float]:
+        id_container = self.__container_docker.id
+        api_docker = self.__api_docker
+        command_orig = command
+        command = self.instrument(command, time_limit, kill_after)
+
+        # create object
+        exec_resp = api_docker.exec_create(id_container, command,
+                                           tty=True,
+                                           stdout=stdout,
+                                           stderr=stderr)
+        exec_id = exec_resp['Id']
+        stream = api_docker.exec_start(exec_id, stream=True)
+        return Popen(command_orig, api_docker, exec_id, stream)
 
     def execute(self,
                 command: str,
