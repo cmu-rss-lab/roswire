297edde6e10c0c440a830f05294decb64f3ba59b
ChrisTimperley <christimperley@googlemail.com>
2019-03-21 13:03:34 -0400

    tidied exception module and added GcovException

--- src/rozzy/exceptions.py
+++ src/rozzy/exceptions.py
@@ -1,31 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains all of Rozzy's exceptions.
+"""
+
+
 class RozzyException(Exception):
-    """
-    Base class used by all Rozzy exceptions.
-    """
+    """Base class used by all Rozzy exceptions."""
 
 
 class RecorderAlreadyStarted(RozzyException):
-    """
-    Recording has already started.
-    """
+    """Recording has already started."""
 
 
 class RecorderNotStarted(RozzyException):
-    """
-    Recording has not begun.
-    """
+    """Recording has not begun."""
 
 
 class RecorderAlreadyStopped(RozzyException):
-    """
-    Recording has already stopped.
-    """
+    """Recording has already stopped."""
 
 
 class ParsingError(RozzyException):
-    """
-    Rozzy failed to parse a given file/string.
-    """
+    """Rozzy failed to parse a given file/string."""
 
 
 class NodeNotFoundError(KeyError, RozzyException):
@@ -37,16 +33,16 @@ class NodeNotFoundError(KeyError, RozzyException):
 
 
 class ServiceNotFoundError(KeyError, RozzyException):
-    """
-    No service was found with the given name.
-    """
+    """No service was found with the given name."""
     def __init__(self, name: str) -> None:
         super().__init__(f"service not found: {name}")
 
 
 class ParameterNotFoundError(KeyError, RozzyException):
-    """
-    No parameter was found with the given name.
-    """
+    """No parameter was found with the given name."""
     def __init__(self, name: str) -> None:
         super().__init__(f"parameter not found: {name}")
+
+
+class GcovException(RozzyException):
+    """An error occurred when running gcov."""
