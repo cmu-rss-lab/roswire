19730d61f9dacfd76a6ab4514af99cb326da3ccf
ChrisTimperley <christimperley@googlemail.com>
2019-04-08 18:42:16 -0400

    updated test_bag

--- test/test_bag.py
+++ test/test_bag.py
@@ -7,14 +7,11 @@ from roswire.definitions.bag import BagReader
 DIR_TEST = os.path.dirname(__file__)
 
 
-# @pytest.mark.skip(reason='not implemented')
 def test_from_file():
     fn_bag = os.path.join(DIR_TEST, 'example.bag')
     bag = BagReader(fn_bag)
     print(f"Index Pos: {bag.header.index_pos}")
     print(f"Conn. Count: {bag.header.conn_count}")
     print(f"Chunk Count: {bag.header.chunk_count}")
-    # print(bag.index)
-    # print(bag.connections)
-    print(bag.read_messages(['/mavros/state']))
-    assert false
+    msgs = list(bag.read_messages(['/mavlink/from']))
+    print(msgs[3])
