1ba6c4a9b861e301dcbb3ff3f19109198cffd820
ChrisTimperley <christimperley@googlemail.com>
2019-04-09 14:20:13 -0400

    removed test_build_type

--- test/test_type_database.py
+++ test/test_type_database.py
@@ -7,24 +7,6 @@ from roswire.definitions import (PackageDatabase, FormatDatabase, TypeDatabase,
 from test_file import build_file_proxy
 
 
-def test_build_type():
-    s = """
-uint32 seq
-time stamp
-string frame_id
-    """
-    fmt = MsgFormat.from_string("PkgName", "MessageName", s)
-    t = TypeDatabase.build_type(fmt)
-    assert t.__name__ == fmt.name
-    assert t.format == fmt
-
-    m = t(seq=310, stamp=Time(30, 120), frame_id='foo')
-
-    # message should be immutable
-    with pytest.raises(attr.exceptions.FrozenInstanceError):
-        m.seq = 500
-
-
 def test_build():
     with build_file_proxy() as files:
         paths = [
