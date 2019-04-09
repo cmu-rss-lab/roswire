e2fa559673768c3a4a56ece297ab09fa9862447d
ChrisTimperley <christimperley@googlemail.com>
2019-04-09 14:58:19 -0400

    added get_builtin

--- src/roswire/definitions/base.py
+++ src/roswire/definitions/base.py
@@ -1,27 +1,8 @@
-__all__ = ('Time', 'is_builtin')
+__all__ = ('Time', 'is_builtin', 'get_builtin')
 
-from typing import Dict, Any, FrozenSet
+from typing import Dict, Any, FrozenSet, Type
 
 import attr
-
-_BUILTIN_TYPES = frozenset({
-    'bool',
-    'int8',
-    'uint8',
-    'int16',
-    'uint16',
-    'int32',
-    'uint32',
-    'int64',
-    'uint64',
-    'float32',
-    'float64',
-    'string',
-    'time',
-    'duration',
-    'char',  # deprecated: alias for uint8
-    'byte'  # deprecated: alias for int8
-})
 
 
 @attr.s(frozen=True, slots=True)
@@ -38,6 +19,31 @@ class Time:
                 'nsecs': self.nsecs}
 
 
+_BUILTIN_TYPES: Dict[str, Type] = {
+    'bool': bool,
+    'int8': int,
+    'uint8': int,
+    'int16': int,
+    'uint16': int,
+    'int32': int,
+    'uint32': int,
+    'int64': int,
+    'uint64': int,
+    'float32': float,
+    'float64': float,
+    'string': str,
+    'time': Time,
+    'duration': None,  # FIXME add Duration
+    'char': int,  # deprecated: alias for uint8
+    'byte': int  # deprecated: alias for int8
+}
+
+
 def is_builtin(typ: str) -> bool:
     """Determines whether a given type is a built-in type."""
     return typ in _BUILTIN_TYPES
+
+
+def get_builtin(typ: str) -> Type:
+    """Returns the Python type that implements a given ROS type."""
+    return _BUILTIN_TYPES[typ]
