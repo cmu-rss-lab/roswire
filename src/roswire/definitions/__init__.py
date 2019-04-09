9263841ab334467cebdd252e77658a149ae692a8
ChrisTimperley <christimperley@googlemail.com>
2019-04-09 18:24:04 -0400

    moved Message to msg module

--- src/roswire/definitions/__init__.py
+++ src/roswire/definitions/__init__.py
@@ -1,7 +1,7 @@
 from .base import Time
-from .msg import Constant, ConstantValue, Field, MsgFormat
+from .msg import Constant, ConstantValue, Field, MsgFormat, Message
 from .srv import SrvFormat
 from .action import ActionFormat
 from .package import Package, PackageDatabase
 from .format import FormatDatabase
-from .type_db import TypeDatabase, Message
+from .type_db import TypeDatabase
