6a3e2f1baf1b44ad4d4e4a949d2492c7fbf55941
Chris Timperley <christimperley@googlemail.com>
2019-04-09 21:12:48 -0400

    Added Duration class (fixes #141) (#142)
    * added Duration class
    
    * fixed whitespace

--- src/roswire/definitions/base.py
+++ src/roswire/definitions/base.py
@@ -1,4 +1,4 @@
-__all__ = ('Time', 'is_builtin', 'get_builtin')
+__all__ = ('Time', 'Duration', 'is_builtin', 'get_builtin')
 
 from typing import Dict, Any, FrozenSet, Type
 
@@ -13,6 +13,20 @@ class Time:
     @staticmethod
     def from_dict(d: Dict[str, Any]) -> 'Time':
         return Time(d['secs'], d['nsecs'])
+
+    def to_dict(self) -> Dict[str, int]:
+        return {'secs': self.secs,
+                'nsecs': self.nsecs}
+
+
+@attr.s(frozen=True, slots=True)
+class Duration:
+    secs: int = attr.ib()
+    nsecs: int = attr.ib()
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> 'Duration':
+        return Duration(d['secs'], d['nsecs'])
 
     def to_dict(self) -> Dict[str, int]:
         return {'secs': self.secs,
