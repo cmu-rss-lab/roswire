5bb9052aa160e71b48dbc40611dce0efc1ded64d
ChrisTimperley <christimperley@googlemail.com>
2019-04-10 11:56:56 -0400

    added type database as argument to BagReader

--- src/roswire/definitions/bag.py
+++ src/roswire/definitions/bag.py
@@ -115,8 +115,9 @@ Index = Dict[int, List[IndexEntry]]
 
 
 class BagReader:
-    def __init__(self, fn: str) -> None:
+    def __init__(self, fn: str, db_type: TypeDatabase) -> None:
         self.__fp = open(fn, 'rb')
+        self.__db_type = db_type
         self.__size_bytes: int = os.path.getsize(fn)
 
         version = self._read_version()
@@ -393,8 +394,10 @@ class BagReader:
         conn_info = self.__connections[conn_id]
         topic = conn_info.topic
         msg_typ_name = conn_info.typ
+        msg_typ = self.__db_type[msg_typ_name]
 
-        # TODO read and decode message content
+        # read and decode message content
+        msg = msg_typ.decode(self.__db_type, bfr)
 
         logger.debug("TOPIC: %s (%s)", topic, msg_typ_name)
         msg = BagMessage(topic, t)
