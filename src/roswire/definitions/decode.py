cb465fa5c868302928f5e4827a4c021446f2f597
ChrisTimperley <christimperley@googlemail.com>
2019-04-10 18:31:37 -0400

    bug fix: bad sed

--- src/roswire/definitions/decode.py
+++ src/roswire/definitions/decode.py
@@ -25,7 +25,7 @@ _SIMPLE_TO_STRUCT = {
 }
 
 
-def decoder_decoder_simple(typ: str) -> Callable[[bytes], Any]:
+def decoder_simple(typ: str) -> Callable[[bytes], Any]:
     """Returns a decoder for a specified simple type."""
     pattern = '<' + _SIMPLE_TO_STRUCT[typ]
 
