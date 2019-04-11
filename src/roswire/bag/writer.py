df7ad055429d249a377d4a0d214de0b6fd22d56b
ChrisTimperley <christimperley@googlemail.com>
2019-04-11 19:30:28 -0400

    added BagWriter class

---
+++ src/roswire/bag/writer.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__all__ = ('BagWriter',)
+
+from .core import BagMessage
+
+
+class BagWriter:
+    """Provides an interface for writing messages to a bag file on disk."""
+    pass
