a4d3373f54367c9d20bbcd0205fb66eec0d13715
ChrisTimperley <christimperley@googlemail.com>
2019-04-11 19:32:20 -0400

    added filename argument

--- src/roswire/bag/writer.py
+++ src/roswire/bag/writer.py
@@ -5,5 +5,17 @@ from .core import BagMessage
 
 
 class BagWriter:
-    """Provides an interface for writing messages to a bag file on disk."""
-    pass
+    """
+    Provides an interface for writing messages to a bag file on disk.
+
+    Attributes
+    ----------
+    filename: str
+        The name of the file to which the bag will be written.
+    """
+    def __init__(self, fn: str) -> None:
+        self.__fn = fn
+
+    @property
+    def filename(self) -> str:
+        return self.__fn
