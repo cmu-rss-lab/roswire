a0a27624ce13ae6d43ea9a858f3a403a77a7ada6
ChrisTimperley <christimperley@googlemail.com>
2019-04-11 18:05:48 -0400

    updated test_bag to use saved format database

--- test/test_bag.py
+++ test/test_bag.py
@@ -1,27 +1,30 @@
 import os
 import logging
 
+import yaml
 import pytest
 
 from roswire.bag import BagReader
+from roswire.definitions import TypeDatabase, FormatDatabase
 
 from test_basic import build_ardu
 
 DIR_TEST = os.path.dirname(__file__)
 
-b'\x00\x00\x00\x00J\xdb\x86\\\xa9\xa4\x11\x0e\x00\x00\x00\x00\x01\x10(\x01\x01o\xaaT\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00)@AT\x02\x00\x00\x00'
+
+def load_mavros_type_db() -> TypeDatabase:
+    fn_db_format = os.path.join(DIR_TEST,
+                                'format-databases/mavros.formats.yml')
+    db_format = FormatDatabase.load(fn_db_format)
+    return TypeDatabase.build(db_format)
 
 
 def test_from_file():
-    log_to_stdout = logging.StreamHandler()
-    log_to_stdout.setLevel(logging.DEBUG)
-    logging.getLogger('roswire.bag').addHandler(log_to_stdout)
-    with build_ardu() as (sut, ros):
-        db_type = sut.description.types
-        fn_bag = os.path.join(DIR_TEST, 'example.bag')
-        bag = BagReader(fn_bag, db_type)
-        print(f"Index Pos: {bag.header.index_pos}")
-        print(f"Conn. Count: {bag.header.conn_count}")
-        print(f"Chunk Count: {bag.header.chunk_count}")
-        msgs = list(bag.read_messages(['/mavlink/from']))
-        print(msgs[3])
+    db_type = load_mavros_type_db()
+    fn_bag = os.path.join(DIR_TEST, 'example.bag')
+    bag = BagReader(fn_bag, db_type)
+    print(f"Index Pos: {bag.header.index_pos}")
+    print(f"Conn. Count: {bag.header.conn_count}")
+    print(f"Chunk Count: {bag.header.chunk_count}")
+    msgs = list(bag.read_messages(['/mavlink/from']))
+    print(msgs[3])
