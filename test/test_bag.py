8d89014a59223701c9346872fdf483eafd7f15f9
ChrisTimperley <christimperley@googlemail.com>
2019-04-12 15:25:42 -0400

    added test_write

--- test/test_bag.py
+++ test/test_bag.py
@@ -1,10 +1,11 @@
 import os
 import logging
+import tempfile
 
 import yaml
 import pytest
 
-from roswire.bag import BagReader
+from roswire.bag import BagReader, BagWriter
 from roswire.definitions import TypeDatabase, FormatDatabase
 
 from test_basic import build_ardu
@@ -19,11 +20,8 @@ def load_mavros_type_db() -> TypeDatabase:
     return TypeDatabase.build(db_format)
 
 
-def test_from_file():
-    db_type = load_mavros_type_db()
+def check_example_bag(db_type: TypeDatabase, bag: BagReader) -> None:
     typ_mavlink = db_type['mavros_msgs/Mavlink']
-    fn_bag = os.path.join(DIR_TEST, 'example.bag')
-    bag = BagReader(fn_bag, db_type)
     assert bag.header.index_pos == 189991
     assert bag.header.conn_count == 7
     assert bag.header.chunk_count == 1
@@ -34,3 +32,30 @@ def test_from_file():
     msgs = list(bag.read_messages(['/mavlink/from']))
     assert all(m.topic == '/mavlink/from' for m in msgs)
     assert all(isinstance(m.message, typ_mavlink) for m in msgs)
+
+
+def test_from_file():
+    db_type = load_mavros_type_db()
+    fn_bag = os.path.join(DIR_TEST, 'example.bag')
+    bag = BagReader(fn_bag, db_type)
+    check_example_bag(db_type, bag)
+
+
+def test_write():
+    db_type = load_mavros_type_db()
+
+    # load the messages from the example bag
+    reader = BagReader(os.path.join(DIR_TEST, 'example.bag'), db_type)
+    messages = list(reader)
+
+    fn_bag = tempfile.mkstemp(suffix='.bag')[1]
+    try:
+        bag = BagWriter(fn_bag)
+        bag.write(messages)
+        bag.close()
+
+        # try to read the bag again
+        reader = BagReader(os.path.join(DIR_TEST, 'example.bag'), db_type)
+        check_example_bag(db_type, reader)
+    finally:
+        os.remove(fn_bag)
