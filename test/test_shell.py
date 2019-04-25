01ac27bbad7c7b4424699c733adecf507d7257ca
Chris Timperley <christimperley@googlemail.com>
2019-04-25 17:42:49 -0400

    Added wait method to Popen (fixes #176) (#180)
    * implemented wait method
    
    * added test_shell
    
    * added assertion to appease mypy
    
    * fixed test name

---
+++ test/test_shell.py
@@ -0,0 +1,18 @@
+import subprocess
+
+import pytest
+
+from roswire.proxy import ShellProxy
+
+from test_basic import build_shell_proxy
+
+
+def test_wait():
+    with build_shell_proxy() as shell:
+        p = shell.popen('sleep 30 && echo "hello world"')
+        with pytest.raises(subprocess.TimeoutExpired):
+            p.wait(1)
+        assert p.wait(35) == 0
+
+        p = shell.popen('sleep 10 && exit 1')
+        assert p.wait() == 1
