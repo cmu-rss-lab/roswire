24ae61336c3b771bf4b75fe66e12dc94157a1cd6
ChrisTimperley <christimperley@googlemail.com>
2019-04-25 11:58:24 -0400

    added placeholder assertions

--- test/test_basic_hw.py
+++ test/test_basic_hw.py
@@ -16,3 +16,7 @@ NAME_IMAGE = 'hello-world'
 def test_description():
     rsw = ROSWire()
     desc = rsw.descriptions.build(NAME_IMAGE, save=False)
+    assert set(desc.packages) == {'something'}
+    assert set(desc.formats.messages) == {'something'}
+    assert set(desc.formats.actions) == {'something'}
+    assert set(desc.formats.services) == {'something'}
