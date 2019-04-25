87f481bc3e7ce314eb6fd69aca433995485528af
ChrisTimperley <christimperley@googlemail.com>
2019-04-25 11:55:39 -0400

    added test_description

--- test/test_basic_hw.py
+++ test/test_basic_hw.py
@@ -10,49 +10,9 @@ import roswire.exceptions
 from roswire import ROSWire, ROSProxy, System, SystemDescription
 from roswire.proxy import ShellProxy, FileProxy, ContainerProxy
 
+NAME_IMAGE = 'hello-world'
 
-@contextlib.contextmanager
-def build_test_environment() -> Iterator[Tuple[System, ROSProxy]]:
+
+def test_description():
     rsw = ROSWire()
-    image = 'hello-world'
-    desc = SystemDescription(image, [], [], [])
-    with rsw.launch(image, desc) as sut:
-        with sut.roscore() as ros:
-            time.sleep(5)
-            yield (sut, ros)
-
-
-@contextlib.contextmanager
-def build_hw() -> Iterator[Tuple[System, ROSProxy]]:
-    rsw = ROSWire()
-    with rsw.launch('hello-world') as sut:
-        with sut.roscore() as ros:
-            time.sleep(5)
-            yield (sut, ros)
-
-
-@contextlib.contextmanager
-def build_shell_proxy() -> Iterator[ShellProxy]:
-    rsw = ROSWire()
-    image = 'hello-world'
-    desc = SystemDescription(image, [], [], [])
-    with rsw.launch(image, desc) as sut:
-        yield sut.shell
-
-
-@contextlib.contextmanager
-def build_file_proxy() -> Iterator[FileProxy]:
-    rsw = ROSWire()
-    image = 'hello-world'
-    desc = SystemDescription(image, [], [], [])
-    with rsw.launch(image, desc) as sut:
-        yield sut.files
-
-
-def test_dumb():
-    with build_test_environment() as (sut, ros):
-        print(ros.topic_to_type)
-
-
-if __name__ == '__main__':
-    test_dumb()
+    desc = rsw.descriptions.build(NAME_IMAGE, save=False)
