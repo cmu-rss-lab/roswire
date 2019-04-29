bc189f2e5b09437d9ff5bb15c2d1e8eb2e7ca963
ChrisTimperley <christimperley@googlemail.com>
2019-04-29 14:27:12 -0400

    flatten message format

--- src/roswire/bag/daikon.py
+++ src/roswire/bag/daikon.py
@@ -3,6 +3,9 @@
 This module provides functionality for converting the contents of ROS bag
 files into Daikon trace (and declaration) files.
 """
+from typing import Dict, Type
+
+from ..definitions import Message, MsgFormat
 from ..description import SystemDescription
 
 
@@ -21,4 +24,11 @@ def build_decls(fn_bag: str,
     sys_desc: SystemDescription
         a description of the system used to produce the bag.
     """
+    # create a program point for each topic
+    topic_to_type: Dict[str, Type[Message]] = {}
+    for topic_name, topic_type in topic_to_type.items():
+        topic_fmt: MsgFormat = topic_type.format
+        for field_ctx, field in topic_fmt.flatten(sys_desc.formats.messages):
+            field_name = '.'.join(field_ctx) + field.name
+
     raise NotImplementedError
