d1df80ad0a111a4db1ea1a9902c9ad8228ff857a
Chris Timperley <christimperley@googlemail.com>
2019-05-07 15:31:59 -0400

    Add method to build Duration from two timestamps (fixes #210) (#211)
    * added between method interface
    
    * added assertion
    
    * implemented method
    
    * bug fix: typo
    
    * added test_duration_between
    
    * added additional test cases
    
    * fixed bugs

---
+++ test/test_primitives.py
@@ -0,0 +1,25 @@
+import pytest
+
+from roswire.definitions.base import Time, Duration
+
+
+def test_duration_between():
+    sec_to_nsec = 1000000000
+    t_start = Time(150, 300)
+    t_stop = Time(150, 350)
+    assert Duration.between(t_start, t_stop) == Duration(0, 50)
+    assert Duration.between(t_start, t_start) == Duration(0, 0)
+    with pytest.raises(AssertionError):
+        Duration.between(t_stop, t_start)
+
+    t_start = Time(150, 300)
+    t_stop = Time(200, 300)
+    assert Duration.between(t_start, t_stop) == Duration(50, 0)
+
+    t_start = Time(150, 300)
+    t_stop = Time(200, 50)
+    assert Duration.between(t_start, t_stop) == Duration(49, sec_to_nsec - 250)
+
+    t_start = Time(150, 50)
+    t_stop = Time(200, 300)
+    assert Duration.between(t_start, t_stop) == Duration(50, 250)
