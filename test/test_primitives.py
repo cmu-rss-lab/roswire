cab8388e121b45b0d930b0e2f9a9cd9f5a8daf8f
ChrisTimperley <christimperley@googlemail.com>
2019-05-07 16:23:38 -0400

    added test_duration_compare

--- test/test_primitives.py
+++ test/test_primitives.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 import pytest
 
 from roswire.definitions.base import Time, Duration
@@ -25,8 +27,7 @@ def test_duration_between():
     assert Duration.between(t_start, t_stop) == Duration(50, 250)
 
 
-def test_time_compare():
-    t = Time
+def check_timelike(t: Type):
     assert t(100, 250) < t(100, 300)
     assert t(100, 250) <= t(100, 300)
     assert not (t(100, 300) < t(100, 250))
@@ -40,3 +41,11 @@ def test_time_compare():
     assert min(t(0, 50), t(100, 200), t(300, 500)) == t(0, 50)
     assert max(t(0, 50), t(100, 200), t(300, 500)) == t(300, 500)
     assert min(t(500, 0), t(100, 200), t(300, 500)) == t(100, 200)
+
+
+def test_time_compare():
+    check_timelike(Time)
+
+
+def test_duration_compare():
+    check_timelike(Duration)
