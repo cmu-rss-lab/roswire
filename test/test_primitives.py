fe94361ee56dd4b03312b5e57b101c11433a5207
Chris Timperley <christimperley@googlemail.com>
2019-05-07 16:26:38 -0400

    Implemented time comparison operations (fixes #212) (#213)
    * added test_time_compare
    
    * added more assertions
    
    * added test_duration_compare

--- test/test_primitives.py
+++ test/test_primitives.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 import pytest
 
 from roswire.definitions.base import Time, Duration
@@ -23,3 +25,27 @@ def test_duration_between():
     t_start = Time(150, 50)
     t_stop = Time(200, 300)
     assert Duration.between(t_start, t_stop) == Duration(50, 250)
+
+
+def check_timelike(t: Type):
+    assert t(100, 250) < t(100, 300)
+    assert t(100, 250) <= t(100, 300)
+    assert not (t(100, 300) < t(100, 250))
+    assert t(100, 300) > t(100, 250)
+    assert t(100, 300) >= t(100, 250)
+
+
+    assert t(100, 250) <= t(100, 300)
+    assert t(500, 0) > t(0, 500)
+
+    assert min(t(0, 50), t(100, 200), t(300, 500)) == t(0, 50)
+    assert max(t(0, 50), t(100, 200), t(300, 500)) == t(300, 500)
+    assert min(t(500, 0), t(100, 200), t(300, 500)) == t(100, 200)
+
+
+def test_time_compare():
+    check_timelike(Time)
+
+
+def test_duration_compare():
+    check_timelike(Duration)
