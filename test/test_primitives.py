36d11c90a2e7d9c730c93486e294d2361c90450b
ChrisTimperley <christimperley@googlemail.com>
2019-05-07 15:28:06 -0400

    added additional test cases

--- test/test_primitives.py
+++ test/test_primitives.py
@@ -4,6 +4,7 @@ from roswire.definitions.base import Time, Duration
 
 
 def test_duration_between():
+    sec_to_nsec = 1000000000
     t_start = Time(150, 300)
     t_stop = Time(150, 350)
     assert Duration.between(t_start, t_stop) == Duration(0, 50)
@@ -14,3 +15,11 @@ def test_duration_between():
     t_start = Time(150, 300)
     t_stop = Time(200, 300)
     assert Duration.between(t_start, t_stop) == Duration(50, 0)
+
+    t_start = Time(150, 300)
+    t_stop = Time(200, 50)
+    assert Duration.between(t_start, t_stop) == Duration(49, sec_to_nsec - 250)
+
+    t_start = Time(150, 50)
+    t_stop = Time(200, 300)
+    assert Duration.between(t_start, t_stop) == Duration(50, 250)
