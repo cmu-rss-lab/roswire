b0509edb3073453a2208d7f96be687bd19b5a510
ChrisTimperley <christimperley@googlemail.com>
2019-05-09 10:43:50 -0400

    added basic command construction

--- src/roswire/proxy/catkin.py
+++ src/roswire/proxy/catkin.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# http://wiki.ros.org/Bags/Format/2.0
 __all__ = ('CatkinProxy',)
 
 from typing import Optional, List
+import shlex
 
 from shell import ShellProxy
 
@@ -15,6 +15,21 @@ class CatkinProxy:
               packages: Optional[List[str]] = None,
               no_deps: bool = False,
               pre_clean: bool = False,
-              jobs: Optional[int] = None
+              jobs: Optional[int] = None,
+              cmake_args: Optional[List[str]] = None,
+              make_args: Optional[List[str]] = None
               ) -> None:
-        pass
+        shell = self._shell
+        command = ['catkin', 'build', '--no-status', '--no-notify']
+        if packages:
+            command += [shlex.quote(p) for p in packages]
+        if no_deps:
+            command += ['--no-deps']
+        if pre_clean:
+            command += ['--pre-clean']
+        if cmake_args:
+            command += [shlex.quote(a) for a in cmake_args]
+        if make_args:
+            command += [shlex.quote(a) for a in make_args]
+
+        # TODO check exit code -- 127 indicates catkin_tools not installed
