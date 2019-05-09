ed9cfdc1d57fffbb26932604c6bb9ccdf6d6d470
ChrisTimperley <christimperley@googlemail.com>
2019-05-09 12:02:59 -0400

    updated test

--- test/test_catkin.py
+++ test/test_catkin.py
@@ -5,8 +5,8 @@ from roswire.proxy import CatkinProxy
 from test_basic import build_hello_world
 
 
-def test_build():
+def test_clean_and_build():
     with build_hello_world() as (sut, ros):
         catkin = sut.catkin('/ros_ws')
+        catkin.clean()
         catkin.build()
-        assert False
