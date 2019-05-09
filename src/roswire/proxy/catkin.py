a6fcfa0eb7b63e735a62042e428b5697262c3d8a
ChrisTimperley <christimperley@googlemail.com>
2019-05-09 10:28:51 -0400

    began outlining build method

--- src/roswire/proxy/catkin.py
+++ src/roswire/proxy/catkin.py
@@ -2,9 +2,19 @@
 # http://wiki.ros.org/Bags/Format/2.0
 __all__ = ('CatkinProxy',)
 
+from typing import Optional, List
+
 from shell import ShellProxy
 
 
 class CatkinProxy:
     def __init__(self, shell: ShellProxy) -> None:
         self._shell = shell
+
+    def build(self,
+              packages: Optional[List[str]] = None,
+              no_deps: bool = False,
+              pre_clean: bool = False,
+              jobs: Optional[int] = None
+              ) -> None:
+        pass
