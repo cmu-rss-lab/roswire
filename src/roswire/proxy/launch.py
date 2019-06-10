cc04189ec5dc1ee075dfbcc66b8a1b4c9622b72f
ChrisTimperley <christimperley@googlemail.com>
2019-06-10 14:30:07 -0400

    parse root node

--- src/roswire/proxy/launch.py
+++ src/roswire/proxy/launch.py
@@ -5,10 +5,10 @@ This file implements a proxy for parsing the contents of launch files.
 __all__ = ('LaunchFileReader',)
 
 import logging
-
-import lxml
+import xml.etree.ElementTree as ET
 
 from .file import FileProxy
+from ..exceptions import FailedToParseLaunchFile
 
 logger: logging.Logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -17,3 +17,37 @@ logger.setLevel(logging.DEBUG)
 class LaunchFileReader:
     def __init__(self, files: FileProxy) -> None:
         self.__files = files
+
+    def read(self, fn: str) -> None:
+        """Parses the contents of a given launch file.
+
+        Reference
+        ---------
+            http://wiki.ros.org/roslaunch/XML/node
+        """
+        root = ET.fromstring(self.__files.read(fn))
+        if root.tag != 'launch':
+            m = 'root of launch file must have <launch></launch> tags'
+            raise FailedToParseLaunchFile(m)
+
+        # TODO handle remap tags
+
+        # TODO handle env tags
+
+        # TODO handle param tags
+
+        # TODO handle include tags: file
+
+        # TODO parse arg tags (name, default, value)
+        for xml_arg in root.findall('arg'):
+            try:
+                name = xml_arg.attrib['name']
+            except:
+                m = "<arg> tag missing 'name' attribute"
+                raise FailedToParseLaunchFile(m)
+
+            logger.debug("found attribute: %s", name)
+
+        # TODO parse rosparam tags
+
+        # TODO parse node tags
