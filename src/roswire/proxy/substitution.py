03d145ec2ed1d7f1b9acced10fa47bf189d572d3
ChrisTimperley <christimperley@googlemail.com>
2019-06-10 18:04:54 -0400

    added param extraction

--- src/roswire/proxy/substitution.py
+++ src/roswire/proxy/substitution.py
@@ -5,12 +5,32 @@ by XML launch and xacro files.
 """
 __all__ = ('resolve',)
 
+import re
+import functools
+import logging
+
 from .shell import ShellProxy
-from .files import FileProxy
+from .file import FileProxy
+
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+R_ARG = re.compile(r'(?<=\$\().+?(?=\))')
+
+
+def resolve_arg(shell: ShellProxy,
+                files: FileProxy,
+                s: str
+                ) -> str:
+    logger.debug("resolving substitution argument: %s", s)
+    kind, *params = s.split(' ')
+    logger.debug("argument kind: %s", kind)
+    return s
 
 
 def resolve(shell: ShellProxy,
             files: FileProxy,
             s: str
             ) -> str:
-    return
+    r = functools.partial(resolve_arg, shell, files)
+    return R_ARG.sub(lambda m: r(m.group(0)), s)
