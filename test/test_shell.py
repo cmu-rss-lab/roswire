0fab47fa2a2ae33bde8d50a84bcaf7718800aa70
Chris Timperley <christimperley@googlemail.com>
2019-06-10 17:06:01 -0400

    Added environ method to shell proxy (fixes #242) (#243)
    * added EnvNotFoundError
    
    * implemented environ
    
    * added test_environ
    
    * added negative test
    
    * check if environment variable exists
    
    * refactor existing code

--- test/test_shell.py
+++ test/test_shell.py
@@ -5,6 +5,7 @@ import os
 import pytest
 
 from roswire.proxy import ShellProxy
+from roswire.exceptions import EnvNotFoundError
 
 from test_basic import build_shell_proxy
 
@@ -25,3 +26,10 @@ def test_local_to_host_pid():
         p = shell.popen('sleep 60')
         pid_host = shell.local_to_host_pid(p.pid)
         assert pid_host is not None
+
+
+def test_environ():
+    with build_shell_proxy() as shell:
+        assert shell.environ('ROS_PACKAGE_PATH')
+        with pytest.raises(EnvNotFoundError):
+            shell.environ('FOOBARFOOBARFOO')
