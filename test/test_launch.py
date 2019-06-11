5cd911d57cc035148120c68eaadd38a83897d624
ChrisTimperley <christimperley@googlemail.com>
2019-06-10 21:19:12 -0400

    updated test

--- test/test_launch.py
+++ test/test_launch.py
@@ -1,9 +1,10 @@
 from roswire.proxy.launch import LaunchFileReader
 
-from test_basic import build_file_proxy
+from test_basic import build_file_and_shell_proxy
 
 
 def test_read():
-    with build_file_proxy() as files:
-        reader = LaunchFileReader(files)
+    with build_file_and_shell_proxy() as (files, shell):
+        reader = LaunchFileReader(shell, files)
         reader.read('/ros_ws/src/mavros/mavros/launch/apm.launch')
+        assert False
