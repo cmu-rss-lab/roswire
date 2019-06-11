a448fd3529949c63c2b6e9d436dc2a323cff1518
ChrisTimperley <christimperley@googlemail.com>
2019-06-11 17:01:37 -0400

    added global_name

--- src/roswire/name.py
+++ src/roswire/name.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 __all__ = (
+    'global_name',
     'name_is_private',
     'name_is_global')
+
+
+def global_name(name: str) -> str:
+    """Converts a name to its canonical global form."""
+    if name_is_private(name):
+        m = f'cannot convert private name [{name}] into global name'
+        raise ValueError(m)
+    if not name_is_global(name):
+        name = '/' + name
+    if name[-1] != '/':
+        name = name + '/'
+    return name
 
 
 def name_is_private(name: str) -> bool:
