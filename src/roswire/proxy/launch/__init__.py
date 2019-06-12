35d52535dc36b25b6242d8132f2d8ce0a2eaa8fc
ChrisTimperley <christimperley@googlemail.com>
2019-06-11 20:48:32 -0400

    added ROSConfig

--- src/roswire/proxy/launch/__init__.py
+++ src/roswire/proxy/launch/__init__.py
@@ -44,6 +44,11 @@ class NodeConfig:
 
 
 @attr.s(frozen=True, slots=True)
+class ROSConfig:
+    nodes: Tuple[str, ...] = attr.ib(default=tuple())
+
+
+@attr.s(frozen=True, slots=True)
 class LaunchContext:
     filename: str = attr.ib()
     resolve_dict: Mapping[str, Any] = attr.ib()
@@ -163,15 +168,16 @@ def tag(name: str, legal_attributes: Collection[str] = tuple()):
     def wrap(loader):
         def wrapped(self,
                     ctx: LaunchContext,
+                    cfg: ROSConfig,
                     elem: ET.Element
-                    ) -> LaunchContext:
+                    ) -> Tuple[LaunchContext, ROSConfig]:
             logger.debug("parsing <%s> tag", name)
             for attribute in elem.attrib:
                 if attribute not in legal_attributes:
                     raise FailedToParseLaunchFile(m)
-            ctx = loader(self, ctx, elem)
+            ctx, cfg = loader(self, ctx, cfg, elem)
             logger.debug("new context: %s", ctx)
-            return ctx
+            return ctx, cfg
         _TAG_TO_LOADER[name] = wrapped
         return wrapped
     return wrap
@@ -192,18 +198,20 @@ class LaunchFileReader:
 
     def _load_tags(self,
                    ctx: LaunchContext,
+                   cfg: ROSConfig,
                    tags: Sequence[ET.Element]
                    ) -> LaunchContext:
         for tag in (t for t in tags if t.tag in _TAG_TO_LOADER):
             loader = _TAG_TO_LOADER[tag.tag]
-            ctx = loader(self, ctx, tag)
-        return ctx
+            ctx, cfg = loader(self, ctx, cfg, tag)
+        return ctx, cfg
 
     @tag('arg', ['name', 'type', 'pkg'])
     def _load_node_tag(self,
                        ctx: LaunchContext,
+                       cfg: ROSConfig,
                        tag: ET.Element
-                       ) -> LaunchContext:
+                       ) -> Tuple[LaunchContext, ROSConfig]:
         name = tag.attrib['name']
         pkg = tag.attrib['type']
         node_type = tag.attrib['type']
@@ -216,30 +224,35 @@ class LaunchFileReader:
                           pkg=pkg,
                           node_type=node_type)
         logger.debug("found node: %s", node)
-        return ctx
+        return ctx, cfg
 
     @tag('arg', ['name', 'default', 'value', 'doc'])
     def _load_arg_tag(self,
                       ctx: LaunchContext,
+                      cfg: ROSConfig,
                       tag: ET.Element
-                      ) -> LaunchContext:
-        return ctx.with_arg(name=tag.attrib['name'],
-                            value=tag.attrib.get('value'),
-                            default=tag.attrib.get('default'),
-                            doc=tag.attrib.get('doc'))
+                      ) -> Tuple[LaunchContext, ROSConfig]:
+        ctx = ctx.with_arg(name=tag.attrib['name'],
+                           value=tag.attrib.get('value'),
+                           default=tag.attrib.get('default'),
+                           doc=tag.attrib.get('doc'))
+        return ctx, cfg
 
     @tag('env', ['name', 'value'])
     def _load_env_tag(self,
                       ctx: LaunchContext,
+                      cfg: ROSConfig,
                       tag: ET.Element
-                      ) -> LaunchContext:
-        return ctx.with_env_arg(tag.attrib['name'], tag.attrib['value'])
+                      ) -> Tuple[LaunchContext, ROSConfig]:
+        ctx = ctx.with_env_arg(tag.attrib['name'], tag.attrib['value'])
+        return ctx, cfg
 
     @tag('include', ['file', 'pass_all_args', 'ns', 'clear_params'])
     def _load_include_tag(self,
                           ctx: LaunchContext,
+                          cfg: ROSConfig,
                           tag: ET.Element
-                          ) -> LaunchContext:
+                          ) -> Tuple[LaunchContext, ROSConfig]:
         include_filename = self._resolve_args(tag.attrib['file'])
         logger.debug("include file: %s", include_filename)
 
@@ -261,10 +274,11 @@ class LaunchFileReader:
 
         # handle child tags
         child_tags = [t for t in tag if t.tag in ('env', 'arg')]
-        ctx_child = self._load_tags(ctx_child, child_tags)
+        ctx_child, cfg = self._load_tags(ctx_child, cfg, child_tags)
         ctx_child = ctx_child.process_include_args()
+        logger.debug("prepared include context: %s", ctx_child)
 
-        return ctx
+        return ctx, cfg
 
     def _handle_ns_and_clear_params(self,
                                     ctx: LaunchContext,
@@ -300,6 +314,7 @@ class LaunchFileReader:
             http://wiki.ros.org/roslaunch/XML/node
             http://docs.ros.org/kinetic/api/roslaunch/html/roslaunch.xmlloader.XmlLoader-class.html
         """
+        cfg = ROSConfig()
         ctx = LaunchContext(namespace='/',
                             filename=fn,
                             resolve_dict={},
@@ -309,4 +324,4 @@ class LaunchFileReader:
             ctx = ctx.with_argv(argv)
 
         launch = self._parse_file(fn)
-        self._load_tags(ctx, list(launch))
+        ctx, cfg = self._load_tags(ctx, cfg, list(launch))
