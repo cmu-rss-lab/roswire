79382a3235239f85b721893a5d5d6c271bcf83b2
ChrisTimperley <christimperley@googlemail.com>
2019-06-11 20:55:58 -0400

    added roslaunch_files

--- src/roswire/proxy/launch/__init__.py
+++ src/roswire/proxy/launch/__init__.py
@@ -47,11 +47,16 @@ class NodeConfig:
 class ROSConfig:
     nodes: Tuple[str, ...] = attr.ib(default=tuple())
     executables: Tuple[str, ...] = attr.ib(default=tuple())
+    roslaunch_files: Tuple[str, ...] = attr.ib(default=tuple())
 
     def with_executable(self, executable: str) -> 'ROSConfig':
         """Specify an executable that should be run at launch."""
         executables = self.executables + (executable,)
         return attr.evolve(self, executables=executables)
+
+    def with_roslaunch_file(self, filename: str) -> 'ROSConfig':
+        roslaunch_files = self.roslaunch_files + (filename,)
+        return attr.evolve(self, roslaunch_files=roslaunch_files)
 
 
 @attr.s(frozen=True, slots=True)
