11fbad01b21201f55bd3c62fe561a3e31ea9995d
ChrisTimperley <christimperley@googlemail.com>
2019-06-14 16:02:07 -0400

    added partial load_radians

--- src/roswire/proxy/launch/rosparam.py
+++ src/roswire/proxy/launch/rosparam.py
@@ -2,3 +2,18 @@
 """
 This file provides utilities for interacting with rosparam.
 """
+
+
+def load_radians(loader, node) -> float:
+    """Safely converts rad(num) to a float value.
+    
+    Note
+    ----
+    This does not support evaluation of expressions.
+    """
+    expr_s = loader.construct_scalar(node).strip()
+    if expr_s.startswith('rad('):
+        expr_s = expr_s[4:-1]
+
+    # TODO safely parse and evaluate expression
+    return float(expr_s)
