fe6afa8023603fe4b2b6042999ef6d26d69d8d5f
ChrisTimperley <christimperley@googlemail.com>
2019-08-07 12:34:46 -0400

    added more to setup.cfg

--- setup.py
+++ setup.py
@@ -10,24 +10,7 @@ with open(path, 'r') as f:
 
 setup(
     version=__version__,
-    python_requires='>=3.6',
-    install_requires=[
-        'attrs>=17.2.0',
-        'typing-extensions>=3.7.2',
-        'ruamel.yaml>=0.15.89',
-        'toposort~=1.5',
-        'docker~=3.7.2',
-        'psutil~=5.6.2',
-        'pyyaml~=5.1'
-    ],
-    setup_requires=[
-        'pytest-runner'
-    ],
-    tests_require=[
-        'pytest'
-    ],
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     test_suites='tests'
 )
