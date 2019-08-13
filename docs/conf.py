7ca17b70835471ceb722ef7ba0a4dba1b9028cc8
ChrisTimperley <christimperley@googlemail.com>
2019-08-12 22:21:41 -0400

    switched back to solar theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,7 @@
 import os
 import sys
 
-# import solar_theme
-# import guzzle_sphinx_theme
+import solar_theme
 
 import roswire
 
@@ -91,8 +90,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
-#html_theme_path = [solar_theme.theme_path]
+html_theme = 'solar_theme'
+html_theme_path = [solar_theme.theme_path]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
