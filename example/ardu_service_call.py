82585f5d81ac9ffe5c6b3940b482dae07830ca43
ChrisTimperley <christimperley@googlemail.com>
2019-09-10 15:01:17 -0400

    use param file and set to guided mode

--- example/ardu_service_call.py
+++ example/ardu_service_call.py
@@ -5,6 +5,7 @@ import roswire
 logging.basicConfig()
 
 FN_SITL = '/ros_ws/src/ArduPilot/build/sitl/bin/arducopter'
+FN_PARAMS = '/ros_ws/src/ArduPilot/copter.parm'
 
 rsw = roswire.ROSWire()
 with rsw.launch('roswire/example:mavros') as system:
@@ -15,7 +16,7 @@ with rsw.launch('roswire/example:mavros') as system:
     with system.roscore() as ros:
         # for this example, we need to separately launch a software-in-the-loop
         # simulator for the robot platform
-        ps_sitl = system.shell.popen(f'{FN_SITL} --model copter')
+        ps_sitl = system.shell.popen(f'{FN_SITL} --model copter --defaults {FN_PARAMS}')
 
         # use roslaunch to launch the application inside the ROS session
         ros.launch('apm.launch', 'mavros', args={'fcu_url': 'tcp://127.0.0.1:5760@5760'})
@@ -25,12 +26,16 @@ with rsw.launch('roswire/example:mavros') as system:
         time.sleep(60)
 
         # disable prearm checking
-        ros.services['/mavros/param/set'].call(request_prearm)
+        # ros.services['/mavros/param/set'].call(request_prearm)
 
         # arm the copter
         request_arm = system.messages['mavros_msgs/CommandBoolRequest'](value=True)
         response_arm = ros.services['/mavros/cmd/arming'].call(request_arm)
         print(response_arm)
+
+        # switch to guided mode
+        request_guided = system.messages['mavros_msgs/SetModeRequest'](base_mode=216, custom_mode='')
+        ros.services['/mavros/set_mode'].call(request_guided)
 
         # takeoff to 50 metres above the ground
         request_takeoff = system.messages['mavros_msgs/CommandTOLRequest'](min_pitch=0.0,
