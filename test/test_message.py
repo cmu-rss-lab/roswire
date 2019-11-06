a8263dac793da885481ced332f4effdee8d0c14d
ChrisTimperley <christimperley@googlemail.com>
2019-11-05 20:14:58 -0500

    added test_to_and_from_dict

--- test/test_message.py
+++ test/test_message.py
@@ -1,4 +1,5 @@
 import pytest
+import yaml
 
 from roswire.definitions import (TypeDatabase, FormatDatabase, MsgFormat,
                                  Time, Duration)
@@ -11,3 +12,36 @@ def test_encode_and_decode():
     Header = db_type['std_msgs/Header']
     orig = Header(seq=32, stamp=Time(secs=9781, nsecs=321), frame_id='')
     assert orig == Header.decode(orig.encode())
+
+
+def test_to_and_from_dict():
+    db_type = load_mavros_type_db()
+    Waypoint = db_type['mavros_msgs/Waypoint']
+    WaypointPushRequest = db_type['mavros_msgs/WaypointPushRequest']
+
+    wp = Waypoint(frame=0,
+                  command=16,
+                  is_current=False,
+                  autocontinue=False,
+                  param1=0.0,
+                  param2=1.0,
+                  param3=2.0,
+                  param4=3.0,
+                  x_lat=4.0,
+                  y_long=5.0,
+                  z_alt=6.0)
+    req = WaypointPushRequest(waypoints=[wp])
+
+    assert req.to_dict() == {'waypoints': [
+        {'frame': 0,
+         'command': 16,
+         'is_current': False,
+         'autocontinue': False,
+         'param1': 0.0,
+         'param2': 1.0,
+         'param3': 2.0,
+         'param4': 3.0,
+         'x_lat': 4.0,
+         'y_long': 5.0,
+         'z_alt': 6.0}
+    ]}
