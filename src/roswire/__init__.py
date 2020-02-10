3faf75da231d5b08983f7a3e95dfd49ee1e6273f
ChrisTimperley <christimperley@googlemail.com>
2020-02-10 09:59:22 -0500

    disable loguru output by default

--- src/roswire/__init__.py
+++ src/roswire/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from loguru import logger as _logger
+logger.disable('roswire')
+
 from .version import __version__
 from .proxy import ShellProxy, ROSProxy
 from .system import System, SystemDescription
