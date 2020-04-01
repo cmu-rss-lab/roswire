fb69b90d7f04821282f7940799ad9d53a9c4c0e3
ChrisTimperley <christimperley@googlemail.com>
2020-03-31 20:29:50 -0400

    added explanation of sources to recipe

--- docs/recipes/describe_ros_package.py
+++ docs/recipes/describe_ros_package.py
@@ -1,6 +1,11 @@
 import roswire
 
 rsw = roswire.ROSWire()
+
+# the list of sources that should be used to setup the catkin workspace
+# in the example list of sources below, we first load the system's workspace
+# (/opt/ros/indigo/setup.bash) before loading the setup script for the devel
+# workspace in /ros_ws
 sources = ['/opt/ros/indigo/setup.bash', '/ros_ws/devel/setup.bash']
 description = rsw.descriptions.load_or_build('roswire/example:mavros', sources)
 package = description.packages['tf2_msgs']
