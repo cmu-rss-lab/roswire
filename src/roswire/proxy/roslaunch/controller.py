cc3e1c408fe558c60e13ab3e082b7dacabacaca7
ChrisTimperley <christimperley@googlemail.com>
2020-05-29 17:22:13 -0400

    added terminate method

--- src/roswire/proxy/roslaunch/controller.py
+++ src/roswire/proxy/roslaunch/controller.py
@@ -8,3 +8,7 @@ import dockerblade
 @attr.s(frozen=True, slots=True, auto_attribs=True)
 class ROSLaunchController:
     _popen: dockerblade.popen.Popen
+
+    def terminate(self) -> None:
+        """Terminates this roslaunch process."""
+        self._popen.terminate()
