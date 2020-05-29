040756bbfb245ff8391573c6ae244888de5749c4
ChrisTimperley <christimperley@googlemail.com>
2020-05-29 17:25:09 -0400

    added filename to controller

--- src/roswire/proxy/roslaunch/controller.py
+++ src/roswire/proxy/roslaunch/controller.py
@@ -7,7 +7,15 @@ import dockerblade
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
 class ROSLaunchController:
-    _popen: dockerblade.popen.Popen
+    """Provides an interface to a roslaunch process.
+
+    Attributes
+    ----------
+    filename: str
+        The absolute path of the XML launch file used by this process.
+    """
+    filename: str
+    _popen: dockerblade.popen.Popen = attr.ib(repr=False)
 
     def terminate(self) -> None:
         """Terminates this roslaunch process."""
