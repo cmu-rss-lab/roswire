4a99c806cb19e4f61cab9977111b4615e70fc0fd
ChrisTimperley <christimperley@googlemail.com>
2020-06-11 15:32:19 -0400

    added test_remappings

--- test/test_roslaunch.py
+++ test/test_roslaunch.py
@@ -22,3 +22,28 @@ def test_read(sut):
                                'points_downsample',
                                'cmd_vel_mux'}
         assert actual_node_names == expected_node_names
+
+
+@pytest.mark.parametrize('sut', ['fetch'], indirect=True)
+def test_remappings(sut):
+    with sut.roscore() as ros:
+        launch = ros.roslaunch('pickplace_playground.launch',
+                               package='fetch_gazebo',
+                               node_to_remappings={'gazebo': [('model_states', '/funkybits')]})
+
+        time.sleep(30)
+        state = ros.state
+        print(state)
+        launch.terminate()
+        time.sleep(5)
+        print('\n'.join(launch.popen.stream))
+        expected_nodes = {'/cmd_vel_mux',
+                          '/gazebo',
+                          '/head_camera/crop_decimate',
+                          '/head_camera/depth_downsample/points_downsample',
+                          '/head_camera/head_camera_nodelet_manager',
+                          '/robot_state_publisher',
+                          '/rosout'}
+        assert state.nodes == expected_nodes
+        assert '/gazebo/model_states' not in state.publishers
+        assert state.publishers['/funkybits'] == {'/gazebo'}
