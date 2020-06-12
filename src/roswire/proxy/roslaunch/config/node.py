c03e9730328a43b931c2443e6f1f3b9c62a4ebef
ChrisTimperley <christimperley@googlemail.com>
2020-06-12 11:45:55 -0400

    added with_launch_prefix to NodeConfig

--- src/roswire/proxy/roslaunch/config/node.py
+++ src/roswire/proxy/roslaunch/config/node.py
@@ -30,6 +30,9 @@ class NodeConfig:
     def full_name(self) -> str:
         return namespace_join(self.namespace, self.name)
 
+    def with_launch_prefix(self, launch_prefix: str) -> 'NodeConfig':
+        return self.with_launch_prefix(launch_prefix)
+
     def with_remappings(self,
                         remappings: Collection[Tuple[str, str]],
                         *,
