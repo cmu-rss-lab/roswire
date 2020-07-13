f77ac70967823054c1171f3cd214d4ce259263e6
Mehal Kashyap <mehalkashyap@gmail.com>
2020-07-13 09:43:13 -0400

    Moved proxy/roslaunch/ros2launch.py to ros2/launch.py and cosequently fixed ros2/ros2.py

--- src/roswire/ros2/ros2.py
+++ src/roswire/ros2/ros2.py
@@ -9,7 +9,7 @@ from .node_manager import ROS2NodeManager
 from .service_manager import ROS2ServiceManager
 from .state import ROS2StateProbe
 from ..proxy import SystemState
-from ..proxy.ros22launch import ROS2LaunchManager
+from .launch import ROS2LaunchManager
 
 if typing.TYPE_CHECKING:
     from ..app import AppInstance
@@ -22,17 +22,17 @@ class ROS2:
     nodes: ROS2NodeManager = attr.ib(init=False)
     services: ROS2ServiceManager = attr.ib(init=False)
     _state_probe: ROS2StateProbe = attr.ib(init=False)
-    launch_manager: ROS2LaunchManager = attr.ib(init=False)
+    launch: ROS2LaunchManager = attr.ib(init=False)
 
     def __attrs_post_init__(self) -> None:
         nodes = ROS2NodeManager.for_app_instance(self.app_instance)
         services = ROS2ServiceManager.for_app_instance(self.app_instance)
         state_probe = ROS2StateProbe.for_app_instance(self.app_instance)
-        launch_manager = ROS2LaunchManager.for_app_instance(self.app_instance)
+        launch = ROS2LaunchManager.for_app_instance(self.app_instance)
         object.__setattr__(self, 'nodes', nodes)
         object.__setattr__(self, 'services', services)
         object.__setattr__(self, '_state_probe', state_probe)
-        object.__setattr__(self, 'launch_manager', launch_manager)
+        object.__setattr__(self, 'launch', launch)
 
     @classmethod
     def for_app_instance(cls, app_instance: 'AppInstance') -> 'ROS2':
