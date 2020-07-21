79760f250d711ae1540b1daa8778957f62837d24
afsafzal <afs.afzal@gmail.com>
2020-07-20 21:17:10 -0400

    Adding executable info to NodeConfig (fixes #385) (#386)
    * Adding executable file info to NodeConfig
    
    * Adding locate_node_binary
    
    * initialize NodeConfig with executable info
    
    * forgotten imports and indentation problems
    
    * Better command to find package and read binary files
    
    * Remove todo
    
    * Fix mypy's problem
    
    * Fix flake8 problems
    
    * Making requested changes
    
    * model checking safe

--- src/roswire/proxy/roslaunch/config/node.py
+++ src/roswire/proxy/roslaunch/config/node.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-__all__ = ('NodeConfig',)
+__all__ = ('NodeConfig', 'ExecutableType',)
 
 from typing import Collection, Optional, Sequence, Tuple
 import xml.etree.ElementTree as ET
+from enum import Enum
 
 import attr
 
 from ....name import namespace_join
+
+
+class ExecutableType(Enum):
+    PYTHON = 1
+    LIKELY_CPP = 2
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
@@ -15,6 +21,8 @@ class NodeConfig:
     name: str
     typ: str
     package: str
+    executable_path: str
+    executable_type: ExecutableType
     remappings: Collection[Tuple[str, str]] = attr.ib(default=tuple())
     filename: Optional[str] = attr.ib(default=None)
     output: Optional[str] = attr.ib(default=None)
