7d8b42ac08d3cc02c2d2a7bf1b97fc62cd51ab9f
ChrisTimperley <christimperley@googlemail.com>
2020-09-29 14:09:16 -0400

    added test_ordering

---
+++ test/test_distribution.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from roswire.distribution import ROSDistribution
+
+
+def test_ordering():
+    assert ROSDistribution.FOXY != ROSDistribution.ELOQUENT
+    assert ROSDistribution.NOETIC != ROSDistribution.FOXY
+
+    # compare ROS2 versions
+    assert ROSDistribution.FOXY > ROSDistribution.ELOQUENT
+    assert ROSDistribution.FOXY >= ROSDistribution.ELOQUENT
+    assert ROSDistribution.ELOQUENT < ROSDistribution.ELOQUENT
+    assert ROSDistribution.ELOQUENT <= ROSDistribution.ELOQUENT
+
+    # compare ROS1 versions
+    assert ROSDistribution.NOETIC > ROSDistribution.KINETIC
+    assert ROSDistribution.NOETIC >= ROSDistribution.KINETIC
+    assert ROSDistribution.KINETIC < ROSDistribution.NOETIC
+    assert ROSDistribution.KINETIC <= ROSDistribution.NOETIC
+
+    # compare ROS1 vs. ROS2
+    with pytest.raises(ValueError):
+        ROSDistribution.FOXY < ROSDistribution.KINETIC
+
+    # sorting
+    distributions = [
+        ROSDistribution.KINETIC,
+        ROSDistribution.INDIGO,
+        ROSDistribution.JADE,
+        ROSDistribution.MELODIC]
+
+    expected_ascending = ['INDIGO', 'JADE', 'KINETIC', 'MELODIC']
+    expected_descending = reversed(expected_ascending)
+    assert [d.name for name in sorted(distributions)] == expected_ascending
+    assert [d.name for name in sorted(distributions, reverse=True)] == expected_ascending
