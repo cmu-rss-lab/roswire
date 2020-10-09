07321d033c1ae53729b0ea8aa3105083b7f7ebff
schmerl <schmerl@cs.cmu.edu>
2020-10-10 12:25:23 +1300

    Moved processing of ExecuteProcess into launch_extractor

--- src/roswire/ros2/reader.py
+++ src/roswire/ros2/reader.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import os
 import shlex
@@ -13,64 +14,50 @@ from ..proxy.roslaunch.context import LaunchContext
 from ..proxy.roslaunch.reader import LaunchFileReader
 
 if typing.TYPE_CHECKING:
-    from ..app import AppInstance
+    from .. import AppInstance
 
 
-@attr.s(eq=False)
+@attr.s(auto_attribs=True)
 class ROS2LaunchFileReader(LaunchFileReader):
 
-    app_instance: 'AppInstance'
+    _app_instance: 'AppInstance'
 
     def locate_node_binary(self, package: str, node_type: str) -> str:
-        pass
+        raise NotImplementedError
 
     def read(self,
              filename: str,
              argv: Optional[Sequence[str]] = None
              ) -> LaunchConfig:
-        """Produces a summary of the effects of a launch file.
+        """
+        Produces a summary of the effects of a launch file.
 
-           Parameters
-           ----------
-           filename: str
-               The name of the launch file, or an absolute path to the launch
-               file inside the container.
-           package: str, optional
-               The name of the package to which the launch file belongs.
-           argv: Sequence[str], optional
-               An optional sequence of command-line arguments that should be
-               supplied to :code:`roslaunch`.
+        Parameters
+        ----------
+        filename: str
+            The name of the launch file, or an absolute path to the launch
+            file inside the container.
+        argv: Sequence[str], optional
+            An optional sequence of command-line arguments that should be
+            supplied to :code:`roslaunch`.
 
-           Raises
-           ------
-           PackageNotFound
-               If the given package could not be found.
-           LaunchFileNotFound
-               If the given launch file could not be found in the package.
-           """
-
-        # Copy resources/launch_extractor._py as a python
-        # file into the container
+        Raises
+        ------
+        LaunchFileNotFound
+            If the given launch file could not be found in the package.
+        """
+        if self._app_instance is None:
+            raise ValueError('ROS2 version requires and AppInstance object')
         logger.debug("Copying launch extraction script")
-        files = self.app_instance.files
-        host_script = pkg_resources. \
-            resource_filename('roswire',
-                              'resources/launch_extractor._py')
-        logger.debug(f'Resource is {host_script}')
+        files = self._app_instance.files
+        host_script = pkg_resources.resource_filename(
+            'roswire',
+            'resources/launch_extractor._py')
         files.copy_from_host(host_script,
                              '/launch_extractor.py')
 
-        # Runs the script using app_instance.shell.
-        output = shlex.quote(os.path.basename(filename) + '.json')
-        cmd = f'python3 /launch_extractor.py --output' \
-              f' {output} {shlex.quote(filename)}'
-        logger.debug(f"Running the script in the container: {cmd}")
-        self.app_instance.shell.check_call(cmd)
-        logger.debug(f"Reading {output} on container")
-        config_json = files.read(output)
-        config_nodes = json.loads(config_json)
-        # Convert json to a launch config
-        # TODO Implement launch config
+        config_nodes = self.process_launch_on_app_instance(filename, files)
+
         lc = LaunchConfig()
         ctx = LaunchContext(namespace='/', filename=filename)
         if argv:
@@ -81,23 +68,30 @@ class ROS2LaunchFileReader(LaunchFileReader):
         logger.debug(f'launch configuration: {cfg}')
         return lc
 
-    def _load_launch_objects(self, ctx: LaunchContext,
+    def process_launch_on_app_instance(self,
+                                       filename: str,
+                                       files: Any) -> Sequence[Dict[str, Any]]:
+        assert self._app_instance is not None
+        output = shlex.quote(os.path.basename(filename) + '.json')
+        cmd = f'python3 /launch_extractor.py --output' \
+              f' {output} {shlex.quote(filename)}'
+        logger.debug(f"Running the script in the container: {cmd}")
+        self._app_instance.shell.check_call(cmd)
+        logger.debug(f"Reading {output} on container")
+        config_json = files.read(output)
+        config_nodes = json.loads(config_json)
+        return config_nodes
+
+    def _load_launch_objects(self,
+                             ctx: LaunchContext,
                              cfg: LaunchConfig,
-                             node_list: Sequence[Sequence[Dict]]
+                             node_list: Sequence[Sequence[Dict[str, Any]]]
                              ) -> Tuple[LaunchContext, LaunchConfig]:
         for nodes in node_list:
             for node in nodes:
                 if node['__TYPE__'] == 'Node':
-                    print(f'processing: {node}')
-                    # remappings = self.\
-                    #     _get_remappings_from_json(node.get('remappings'))
-                    args: Optional[Any] = node.get('args', [])
-                    if isinstance(args, list):
-                        args = ' '.join(args)
-                    print(args)
-                    remappings = node.get('remappings', [])
-                    if remappings is not None:
-                        remappings = tuple(remappings)
+                    args = ' '.join(node.get('args', []))
+                    remappings = tuple(node.get('remappings', []))
                     nc = NodeConfig(
                         name=node['name'],
                         namespace=node['namespace'],
@@ -106,36 +100,22 @@ class ROS2LaunchFileReader(LaunchFileReader):
                         executable_type=ExecutableType[
                             node['executable_type']
                         ],
-                        remappings=remappings, # noqa
+                        remappings=remappings,
                         filename=node.get('filename'),
                         output=node.get('output'),
-                        required=self._get_bool(node.get('required'), False),
-                        respawn=self._get_bool(node.get('respawn'), False),
-                        respawn_delay=self._get_float(
-                            node.get('respawn_delay'), 0.0),
-                        env_args=tuple(node.get('env_args', [])), # noqa
+                        required=node.get('required', False),
+                        respawn=node.get('respawn', False),
+                        respawn_delay=node.get('respawn_delay', 0.0),
+                        env_args=tuple(node.get('env_args', [])),
                         cwd=node.get('cwd'),
                         args=args,
                         launch_prefix=node.get('launch_prefix'),
-                        typ=''
+                        # ROS 2 has no type in nodes, derive it from
+                        # the fulle executable path
+                        typ=os.path.basename(node['executable_path'])
                     )
-                    cfg.with_node(nc)
+                    cfg = cfg.with_node(nc)
                 elif node['__TYPE__'] == 'ExecuteProcess' \
                         and node['cmd'][0] == 'gazebo':
-                    nc = NodeConfig(
-                        name='gazebo',
-                        namespace='',
-                        typ='',
-                        package='',
-                        executable_path=node['cmd'][0],
-                        executable_type=ExecutableType.LIKELY_CPP,
-                        args=' '.join(node['cmd'][1:]),
-                    )
-                    cfg.with_node(nc)
+                    raise NotImplementedError
         return ctx, cfg
-
-    def _get_bool(self, v: Optional[Any], _default: bool) -> bool:
-        return bool(v) if v is not None else _default
-
-    def _get_float(self, v: Optional[Any], _default: float) -> float:
-        return float(v) if v is not None else _default
