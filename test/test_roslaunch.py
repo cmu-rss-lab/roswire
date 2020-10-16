85b53380841408bf6dce786cf0443b275bbd0928
ChrisTimperley <christimperley@googlemail.com>
2020-10-16 17:11:14 -0400

    added skip to ROS2 tests

--- test/test_roslaunch.py
+++ test/test_roslaunch.py
@@ -62,7 +62,7 @@ def test_remappings(sut):
         assert set(state.publishers['/funkybits']) == {'/gazebo'}
 
 
-
+@pytest.mark.skip(reason='ROS2 is not fully supported')
 @pytest.mark.parametrize('sut', ['turtlebot3-ros2'], indirect=True)
 def test_locate_with_package_ros2(sut):
     actual_path = sut.ros2.launch.locate('cartographer.launch.py', package='turtlebot3_cartographer')
@@ -70,6 +70,7 @@ def test_locate_with_package_ros2(sut):
     assert actual_path == expected_path
 
 
+@pytest.mark.skip(reason='ROS2 is not fully supported')
 @pytest.mark.parametrize('sut', ['turtlebot3-ros2'], indirect=True)
 def test_launch_ros2(sut):
     actual_controller_command = sut.ros2.launch.launch('cartographer.launch.py', package='turtlebot3_cartographer').popen.args
@@ -77,9 +78,9 @@ def test_launch_ros2(sut):
     assert actual_controller_command == expected_controller_command
 
 
+@pytest.mark.skip(reason='ROS2 is not fully supported')
 @pytest.mark.parametrize('sut', ['turtlebot3-ros2'], indirect=True)
 def test_launch_with_full_path(sut):
     actual_controller_command = sut.ros2.launch.launch('/ros_ws/src/turtlebot3/turtlebot3_cartographer/launch/cartographer.launch.py', package='turtlebot3_cartographer').popen.args
     expected_controller_command = 'ros2 launch turtlebot3_cartographer cartographer.launch.py'
     assert actual_controller_command == expected_controller_command
-
