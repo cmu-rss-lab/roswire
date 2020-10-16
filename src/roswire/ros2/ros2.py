eabd6d57a04096ffc7afd56ce59633fd4b49256f
schmerl <schmerl@cs.cmu.edu>
2020-10-17 09:52:23 +1300

    Review comments

--- src/roswire/ros2/ros2.py
+++ src/roswire/ros2/ros2.py
@@ -12,7 +12,7 @@ from .state import ROS2StateProbe
 from ..proxy import SystemState
 
 if typing.TYPE_CHECKING:
-    from ..app import AppInstance
+    from ... import AppInstance
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
