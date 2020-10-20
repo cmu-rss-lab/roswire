4694283c0087c5594bf9a2551934ba1f48820a3b
schmerl <schmerl@cs.cmu.edu>
2020-10-21 09:27:13 +1300

    Optimized inputs

--- src/roswire/ros1/__init__.py
+++ src/roswire/ros1/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
-# from .node import ROS1Node
-# from .node_manager import ROS1NodeManager
-# from .parameter import ParameterServer
 from .ros1 import ROS1
-# from .roslaunch import ROS1LaunchManager
-# from .service import Service, ServiceManager
-# from .state import SystemStateProbe
