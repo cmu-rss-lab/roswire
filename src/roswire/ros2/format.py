279613a0d145e4a50179747d2ea67236857637b0
schmerl <schmerl@cs.cmu.edu>
2021-02-25 04:31:35 +1300

    Beginning of msgformat refactor

--- src/roswire/ros2/format.py
+++ src/roswire/ros2/format.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 __all__ = ("ROS2FormatDatabase",)
 
-from typing import Any, Dict
+from typing import Any, Dict, Set
 
-from . import ROS2ActionFormat
+from . import ROS2ActionFormat, ROS2MsgFormat
 from ..common import (
     FormatDatabase,
-    MsgFormat,
     SrvFormat,
 )
 
 
-class ROS2FormatDatabase(FormatDatabase[MsgFormat,
+class ROS2FormatDatabase(FormatDatabase[ROS2MsgFormat,
                                         SrvFormat,
                                         ROS2ActionFormat]):
 
     @classmethod
     def from_dict(cls, d: Dict[str, Any]) -> "FormatDatabase":
         """Loads a format database from a JSON document."""
-        msg = {MsgFormat.from_dict(dd) for dd in d["messages"]}
+        msg: Set[ROS2MsgFormat] = {
+            ROS2MsgFormat.from_dict(dd) for dd in d["messages"]
+        }
         srv = {SrvFormat.from_dict(dd) for dd in d["services"]}
         action = {ROS2ActionFormat.from_dict(dd) for dd in d["actions"]}
         return ROS2FormatDatabase(msg, srv, action)
