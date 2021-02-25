e68d414400d227c6b8c8b7e962a3735aa87c7b8e
schmerl <schmerl@cs.cmu.edu>
2021-02-26 06:54:56 +1300

    Redundant import

--- src/roswire/ros2/format.py
+++ src/roswire/ros2/format.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __all__ = ("ROS2FormatDatabase",)
 
-from typing import Any, Dict, Set
+from typing import Any, Dict
 
 from . import ROS2ActionFormat
 from ..common import (
