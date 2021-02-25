8d312c684f8d7dc996e68d44ec0524a6457141b8
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-02-26 07:16:50 +1300

    Undo MsgFormat checkin done by mistake (#455)
    * Undo MsgFormat checkin done by mistake
    
    * Redundant import
    
    * formatting
    
    * typing

--- src/roswire/ros2/format.py
+++ src/roswire/ros2/format.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 __all__ = ("ROS2FormatDatabase",)
 
-from typing import Any, Dict, Set
+from typing import Any, Dict
 
-from . import ROS2ActionFormat, ROS2MsgFormat
+from . import ROS2ActionFormat
 from ..common import (
     FormatDatabase,
+    MsgFormat,
     SrvFormat,
 )
 
 
-class ROS2FormatDatabase(FormatDatabase[ROS2MsgFormat,
+class ROS2FormatDatabase(FormatDatabase[MsgFormat,
                                         SrvFormat,
                                         ROS2ActionFormat]):
 
     @classmethod
     def from_dict(cls, d: Dict[str, Any]) -> "FormatDatabase":
         """Loads a format database from a JSON document."""
-        msg: Set[ROS2MsgFormat] = {
-            ROS2MsgFormat.from_dict(dd) for dd in d["messages"]
-        }
+        msg = {MsgFormat.from_dict(dd) for dd in d["messages"]}
         srv = {SrvFormat.from_dict(dd) for dd in d["services"]}
         action = {ROS2ActionFormat.from_dict(dd) for dd in d["actions"]}
         return ROS2FormatDatabase(msg, srv, action)
