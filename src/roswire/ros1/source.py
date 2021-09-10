1a96226f7d5994cdb25e5c3924f762058415547e
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-09-10 11:01:14 -0400

    Add package to nodelet error message

--- src/roswire/ros1/source.py
+++ src/roswire/ros1/source.py
@@ -47,7 +47,8 @@ class ROS1PackageSourceExtractor(CMakeExtractor):
         nodelets = self.get_nodelet_entrypoints(package)
         for nodelet, entrypoint in nodelets.items():
             if nodelet not in info.targets:
-                logger.error(f"'{nodelet}' is referenced in "
+                logger.error(f"Package {package.name}: '{nodelet}' "
+                             f"is referenced in "
                              f"nodelet_plugins.xml but not in "
                              f"CMakeLists.txt")
             else:
