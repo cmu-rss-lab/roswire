a80e688fb11fcd4de52174ec4e5d0df19812c688
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-08 13:15:07 -0400

    Exporting packages

--- src/roswire/common/package_xml/__init__.py
+++ src/roswire/common/package_xml/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 # This module is largely a copy of https://github.com/ros-infrastructure/catkin_pkg/tree/master/src/catkin_pkg
+from .package import PackageDefinition, parse_package_string
