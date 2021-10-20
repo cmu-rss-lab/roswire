063de46dfc295fd189e7742c53d490ba07e50158
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-20 15:33:22 -0400

    Replace iteritems with items

--- src/roswire/common/cmake.py
+++ src/roswire/common/cmake.py
@@ -271,7 +271,7 @@ class ParserContext(object):
                 yield (cmd, args, arg_tokens, loc)
         finally:
             self._call_stack.remove(lname)
-            for key, value in dict.iteritems(save_vars):
+            for key, value in dict.items(save_vars):
                 if value is not None:
                     var[key] = value
                 else:
